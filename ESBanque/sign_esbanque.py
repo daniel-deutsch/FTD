f9db4017f96c6ac1375957155fd57c08dcc2001e
daniel-deutsch <danieljorged1@gmail.com>
2021-10-02 23:52:31 +0200

    almost finished fe ea2

--- ESBanque/sign_esbanque.py
+++ ESBanque/sign_esbanque.py
@@ -1,18 +1,19 @@
-from bs4 import BeautifulSoup
-from requests import Session
+# from bs4 import BeautifulSoup
+# from requests import Session
 
-s = Session()
+# s = Session()
 
-r = s.get("http://campus.esbanque.fr/c")
-soup = BeautifulSoup(r.text, 'html.parser')
+# r = s.get("http://campus.esbanque.fr/c")
+# soup = BeautifulSoup(r.text, 'html.parser')
 
 
+# s.post(
+#     "https://campus.esbanque.fr/extra/Extranet?page=calendrier",
+#     params={
+#         'action': "saveEmargement",
+#         'signature': "base_64 da assinatura",
+#         'session': "session key 276132$0$15/09/2021$13H30-17H00$MFIN-UNI-100$2122-PLS-G01"
+#     }
+# )
 
-s.post(
-    "https://campus.esbanque.fr/extra/Extranet?page=calendrier",
-    params={
-        'action': "saveEmargement",
-        'signature': "base_64 da assinatura",
-        'session': "session key 276132$0$15/09/2021$13H30-17H00$MFIN-UNI-100$2122-PLS-G01"
-    }
-)
+
